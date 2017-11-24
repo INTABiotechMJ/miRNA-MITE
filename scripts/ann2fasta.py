4782816f1e02cc125a253d9bd98465f7dd33c795
juan <juan@Juans-MacBook-Pro.local>
2017-11-24 09:30:28 -0300

    initial

--- scripts/ann2fasta.py
+++ scripts/ann2fasta.py
@@ -5,7 +5,7 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 
-def fasta_from_ann(annotation, sequence, feature, output_fasta):
+def fasta_from_ann(annotation, sequence, feature, windows, output_fasta):
     df_gff = pd.read_csv(annotation, index_col=False, sep='\t',header=None)
     df_gff.columns = ['seqname' , 'source' , 'feature' , 'start' , 'end' , 'score' , 'strand' , 'frame' , 'attribute']
 
@@ -15,7 +15,15 @@ def fasta_from_ann(annotation, sequence, feature, output_fasta):
         df_exctract = df_gff[(df_gff.seqname == record.id) & (df_gff.feature == feature)]
         for k,v in df_exctract.iterrows():
             clean_seq = ''.join(str(record.seq).splitlines())
-            new_seq = clean_seq[int(v.start):int(v.end)]
+            if int(v.start) - windows < 0:
+                start = 0
+            else:
+                start = int(v.start) - windows
+            if int(v.end) + windows > len(clean_seq):
+                end = len(clean_seq)
+            else:
+                end = int(v.end) + windows
+            new_seq = clean_seq[start:end]
             new_id = record.id + "_from_" + str(v.start) + "_to_" + str(v.end) + "_feature_" + v.feature
             desc = "attribute: " + v.attribute + " strand: " + v.strand
             seq = SeqRecord(Seq(new_seq), id=new_id ,description = desc)
@@ -28,6 +36,7 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--annotation", help="Annotation file (.gff3 format)",  required=True)
     parser.add_argument("-s", "--sequence", help="Sequence file (.fasta)",  required=True)
     parser.add_argument("-f", "--feature", help="Feature to extract (ie. gene)",  required=True)
+    parser.add_argument("-w", "--windows", help="+- nt to cut from and to",  required=True)
     parser.add_argument("-o", "--output", help="Output file name (.fasta format)",  required=True)
     args = parser.parse_args()
-    fasta_from_ann(args.annotation, args.sequence, args.feature, args.output)
+    fasta_from_ann(args.annotation, args.sequence, args.feature, args.windows, args.output)
