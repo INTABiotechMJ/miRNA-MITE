5331ee896f88659dd7a2e3ef8abc831a0ec005bd
juan <juan@Juans-MacBook-Pro.local>
2017-11-24 10:50:05 -0300

    initial

--- scripts/ann2fasta.py
+++ scripts/ann2fasta.py
@@ -7,8 +7,7 @@ from Bio.Seq import Seq
 
 def fasta_from_ann(annotation, sequence, feature, windows, output_fasta):
     df_gff = pd.read_csv(annotation, index_col=False, sep='\t',header=None)
-    df_gff.columns = ['seqname' , 'source' , 'feature' , 'start' , 'end' , 'score' , 'strand' , 'frame' , 'attribute']
-
+    df_gff.columns = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
     fasta_seq = SeqIO.parse(sequence,'fasta')
     buffer = []
     for record in fasta_seq:
@@ -26,7 +25,7 @@ def fasta_from_ann(annotation, sequence, feature, windows, output_fasta):
             new_seq = clean_seq[start:end]
             new_id = record.id + "_from_" + str(v.start) + "_to_" + str(v.end) + "_feature_" + v.feature
             desc = "attribute: " + v.attribute + " strand: " + v.strand
-            seq = SeqRecord(Seq(new_seq), id=new_id ,description = desc)
+            seq = SeqRecord(Seq(new_seq), id=new_id,description = desc)
             buffer.append(seq)
     SeqIO.write(buffer, output_fasta, "fasta")
 
