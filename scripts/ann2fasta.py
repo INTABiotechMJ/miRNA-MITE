043d43a398c2d0e10575c1e2f74b583eb6c7cc45
juan <juan@Juans-MacBook-Pro.local>
2017-11-27 11:22:32 -0300

    initial

--- scripts/ann2fasta.py
+++ scripts/ann2fasta.py
@@ -5,7 +5,7 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 
-def fasta_from_ann(annotation, sequence, feature, windows, output_fasta):
+def fasta_from_ann(annotation, sequence, windows, output_fasta):
     df_gff = pd.read_csv(annotation, index_col=False, sep='\t',header=None)
     df_gff.columns = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
     fasta_seq = SeqIO.parse(sequence,'fasta')
@@ -34,8 +34,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--annotation", help="Annotation file (.gff3 format)",  required=True)
     parser.add_argument("-s", "--sequence", help="Sequence file (.fasta)",  required=True)
-    parser.add_argument("-f", "--feature", help="Feature to extract (ie. gene)",  required=True)
     parser.add_argument("-w", "--windows", help="+- nt to cut from and to", type=int, required=True)
     parser.add_argument("-o", "--output", help="Output file name (.fasta format)",  required=True)
     args = parser.parse_args()
-    fasta_from_ann(args.annotation, args.sequence, args.feature, args.windows, args.output)
+    fasta_from_ann(args.annotation, args.sequence, args.windows, args.output)
