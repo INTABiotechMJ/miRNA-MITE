c4c9e8337b997ff0437185dd8ffe372d904e701b
juan <juan@Juans-MacBook-Pro.local>
2017-11-29 14:02:33 -0300

    initial

--- scripts/helpers/mirna_expand_reads.py
+++ scripts/helpers/mirna_expand_reads.py
@@ -9,6 +9,7 @@ def mirna_expand(libraries, output_fasta):
     """
     buffer_seqs = []
     count = 0
+    print (libraries)
     for library in libraries:
         lib_file = open(library)
         for line in lib_file:
@@ -23,7 +24,7 @@ def mirna_expand(libraries, output_fasta):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()#pylint: disable=invalid-name
-    parser.add_argument("-l", "--libraries", help="(.fasta)", required=True, type=list)
+    parser.add_argument("-l", "--libraries", help="(.fasta)", required=True, nargs='+')
     parser.add_argument("-o", "--output", help="Output file name (.fasta format)", required=True)
     args = parser.parse_args()#pylint: disable=invalid-name
     mirna_expand(args.libraries, args.output)
