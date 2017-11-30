627f63050542b733f1381da8afea46e82ba0aff5
juan <juan@Juans-MacBook-Pro.local>
2017-11-30 15:08:06 -0300

    initial

--- scripts/FAfilterByLenght.py
+++ scripts/FAfilterByLenght.py
@@ -8,7 +8,7 @@ def filter(sequence, lengths, output_fasta):
     fasta_seq = SeqIO.parse(sequence, 'fasta')
     buffer_seqs = []
     for record in fasta_seq:
-        if len(record.seq) in lengths:
+        if str(len(record.seq)) in lengths:
             buffer_seqs.append(record)
     SeqIO.write(buffer_seqs, output_fasta, "fasta")
 
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()#pylint: disable=invalid-name
     parser.add_argument("-s", "--sequence", help="Sequence file (.fasta)", required=True)
-    parser.add_argument("-l", "--lenghts", help="Exclude some ids", action='append', type='int')
+    parser.add_argument("-l", "--lenghts", help="Exclude some ids", action='append')
     parser.add_argument("-o", "--output", help="Output file name (.fasta format)", required=True)
     args = parser.parse_args()#pylint: disable=invalid-name
     filter(args.sequence, args.lenghts, args.output)
